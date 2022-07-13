53e8d590d94db7341502973aac02e14e2c02b3ae
Lê Mạnh Cường <cuong.dz.1806@gmail.com>
2022-07-13 17:57:33 +0700

    Minh Quan part copied

---
+++ thethao.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/HeaderNavFooter.Master" AutoEventWireup="true" CodeBehind="thethao.aspx.cs" Inherits="BTL_News_Website.thethao" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="asset/css/category_page_style.css" />
+        <title></title>
+    </head>
+    <body>
+        <section class="body" id="body" runat="server">
+
+        </section>
+    </body>
+    </html>
+</asp:Content>
---
+++ xahoi.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BTL_News_Website
+{
+    public partial class xahoi : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            List<News> news = (List<News>)Application["newslist"];
+            List<News> xh = new List<News>();
+
+            foreach (News sp in news)
+            {
+                if (sp.Category.Equals("Xã hội"))
+                {
+                    xh.Add(sp);
+                }
+            }
+
+            string html = "";
+            foreach (News sp in xh)
+            {
+                html += "<section class='item'>" +
+                    "<a href='trangconchitiet.aspx?id=" + sp.Id + "'>" +
+                    "<img src='" + sp.Image + "' />" +
+                    "<p class='title'>" + sp.Title + "</p>" +
+                    "<section class='description'>" +
+                    "<p class='time'>" + sp.Time + "  • </p>" +
+                    "<p class='category'>" + sp.Category + "</p>" +
+                    "</section>" +
+                    "</a>" +
+                    "</section>";
+            }
+            body.InnerHtml = html;
+        }
+    }
+}
